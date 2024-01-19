5dfc43e9549a8fbc93190f3d94fa14b50217a65c
HARRY_LOIC <armandjean93@gmail.com>
2024-01-19 17:10:51 +0000

    Ajout d'une nouvelle fonctionalité à notre application*

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const randomNumber = Math.floor(Math.random() * 100);
+  const randomNumbers = Array.from({ length: 10 }, (_, i) => Math.floor(Math.random() * 100));
 
   res.send({
-    randomNumber: randomNumber,
+    randomNumbers: randomNumbers,
   });
 });
 
